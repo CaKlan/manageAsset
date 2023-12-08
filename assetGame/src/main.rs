5712906209ab3dff21c3b2154a5f51cc5bef916c
까미집사 <42618411+CaKlan@users.noreply.github.com>
2023-12-08 14:40:56 +0000

    플레이어 및 UI 디스플레이 추가

--- assetGame/src/main.rs
+++ assetGame/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+use colored::Colorize;
 
 fn main() {
 
@@ -7,38 +9,43 @@ fn main() {
         println!("캐릭터를 선택하세요.");
     } else {
         println!("캐릭터를 생성하세요 ");
+        let player = create_player();
+        player_list.push(player);
     }
     
-    display_box();
+    display_box(&player_list[0]);
+    player_list[0].show_info();
     let user_response = wait_input();
 }
 
 fn wait_input() -> String{
     let stdin = std::io::stdin();
     let mut user_input = String::new();
-    stdin.read_line(&mut user_input).unwrap();
+    let _ = stdin.read_line(&mut user_input).unwrap();
     user_input
 }
 
-fn display_box(){
-    println!("┌────────────────────────────────────────────────────────────────────────┐");
-    println!("│                                                                        │");
-    println!("│                                                                        │");
-    println!("│                                                                        │");
-    println!("│                                                                        │");
-    println!("│                                                                        │");
-    println!("│                                                                        │");
+fn display_box(player : &Player){
+    println!("┌────────────────────────────────────────────────────────────────────────┐"); 
+    println!("│  {:^width$}  │", player.name, width = 68); //72 공백
+    println!("│  {:^width$}  │", "", width = 68); //72 공백
+    println!("│  {:^width$}  │", "", width = 68); //72 공백
+    println!("│  {:^width$}  │", "", width = 68); //72 공백
+    println!("│  {:^width$}  │", "", width = 68); //72 공백
+    println!("│  {:^width$}  │", "", width = 68); //72 공백
     println!("└────────────────────────────────────────────────────────────────────────┘");
 }
 
+#[derive(Debug)]
 struct GameObjectInfo{
-    level:f64,
+    level:i32,
     hp: f64,
     max_hp: f64,
     mp: f64,
     max_mp: f64,
 }
 
+#[derive(Debug)]
 struct Player{
     name: String,
     info: GameObjectInfo,
@@ -51,10 +58,37 @@ impl Player{
             info,
         }
     }
+
+    fn show_info(&self){
+        println!("  {:^width$}  ", 
+        format!("name : {name} | {lvl} | {hp} | {mp}", 
+            name = self.name, 
+            lvl = format!("lvl : {}", self.info.level).as_str().yellow(), 
+            hp = format!("hp : {}/{}", self.info.hp, self.info.max_hp).as_str().red(), 
+            mp = format!("mp : {}/{}", self.info.mp, self.info.max_mp).as_str().blue()), 
+            width = 97);
+    }
 }
 
 fn create_player() -> Player{
     let stdin = std::io::stdin();
     let mut buf = String::new();
     print!("캐릭터의 이름 : ");
+    let _ = std::io::stdout().flush();
+    stdin.read_line(&mut buf);
+    let name = buf.trim().to_string();
+
+    // print!("캐릭터의 직업 : ");
+    // stdin.read_line(&mut buf);
+    // let name = buf;
+    Player{
+        name: name,
+        info: GameObjectInfo {
+            level: 1,
+            hp: 50.0,
+            max_hp: 50.0,
+            mp: 50.0,
+            max_mp: 50.0,
+        }
+    }
 }
